a261e8ac4a116d6a13d94629daf09a05c0cf655b
Adi Sujiwo <sujiwo@gmail.com>
2020-11-19 09:24:52 +0900

    VLAD class proto

---
+++ include/VLAD.h
@@ -0,0 +1,21 @@
+/*
+ * VLAD.h
+ *
+ *  Created on: Nov 18, 2020
+ *      Author: sujiwo
+ */
+
+#ifndef PLACE_RECOGNIZER_VLAD_H_
+#define PLACE_RECOGNIZER_VLAD_H_
+
+namespace PlaceRecognizer {
+
+class VLAD {
+public:
+	VLAD();
+	virtual ~VLAD();
+};
+
+} /* namespace PlaceRecognizer */
+
+#endif /* PLACE_RECOGNIZER_VLAD_H_ */
---
+++ lib/VLAD.cpp
@@ -0,0 +1,21 @@
+/*
+ * VLAD.cpp
+ *
+ *  Created on: Nov 18, 2020
+ *      Author: sujiwo
+ */
+
+#include <VLAD.h>
+
+namespace PlaceRecognizer {
+
+VLAD::VLAD() {
+	// TODO Auto-generated constructor stub
+
+}
+
+VLAD::~VLAD() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace PlaceRecognizer */
