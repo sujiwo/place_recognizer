6c39c9fe2e8fcf3d6152b2f783b5cb0166cc13cf
Adi Sujiwo <sujiwo@gmail.com>
2020-11-25 11:51:53 +0900

    Installation

--- python/VLAD.pyx
+++ python/VLAD.pyx
@@ -2,14 +2,15 @@
 # Check 
 # https://github.com/cmarshall108/cython-cmake-example.git
 # on how to use CMake with Cython
+# XXX: jsk_recognition also uses Cython, might as well check it out
 
 cdef class VisualDictionaryBinary():
     
-    cpdef __init__ (self, numWords=256, numFeaturesOnImage=3000):
+    def __init__ (self, numWords=256, numFeaturesOnImage=3000):
         pass
 
 
 cdef class bVLAD():
     
-    cpdef __init__ (self, numWords=256, numFeaturesOnImage=3000):
+    def __init__ (self, numWords=256, numFeaturesOnImage=3000):
         pass
