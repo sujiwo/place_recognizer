931bdfd63fbade740d64bfd104ec33bbbc276b97
Adi Sujiwo <sujiwo@gmail.com>
2020-12-01 20:12:18 +0900

    Debug

--- nodes/bkmeans_test.cpp
+++ nodes/bkmeans_test.cpp
@@ -9,6 +9,13 @@ using namespace PlaceRecognizer;
 int main(int argc, char *argv[])
 {
 	BKMeans bkm(256, 20);
+	auto descriptors = npy::loadMat(argv[1]);
+	cout << descriptors.rows << 'x' << descriptors.cols << endl;
+
+	auto i = bkm.cluster(descriptors);
+	cout << "Output: " << i << endl;
+	auto C = bkm.get_centroids();
+	npy::saveMat(C, "/tmp/BKMeans.out.npy");
 
 	return 0;
 }
