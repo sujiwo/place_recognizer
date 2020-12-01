3f748f767a0059d3e1d7e9208c455834af05d4be
Adi Sujiwo <sujiwo@gmail.com>
2020-12-01 16:15:25 +0900

    BKMeans

---
+++ include/BKMeans.h
@@ -0,0 +1,250 @@
+//--------------------------------------
+// BKMeans for compact binary code
+// Taken from: https://github.com/caijimin/BKMeans
+//--------------------------------------
+
+#ifndef _BKMEANS_H_
+#define _BKMEANS_H_ 1
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <stdint.h>
+#include <nmmintrin.h>
+#include <algorithm>
+#include <opencv2/core.hpp>
+
+#define BKMEANS_RET_CONV  1
+#define BKMEANS_RET_MAXIT 2
+
+typedef struct wssestruct {
+    double cost1;
+    double cost2;
+} wssestruct;
+
+/*
+   class BKMeans
+   This class implements kmeans for binary data.
+*/
+
+class BKMeans
+{
+    public:      
+        BKMeans(uint64_t *binary_data, size_t n, size_t bit_width, size_t K, size_t iter_max, 
+                float t_for_assign = 1) { 
+
+            epsilon = 0;
+            _samples = binary_data; 
+            _maxIters = iter_max;
+            _k = K;
+            _n = n;
+            if ((bit_width % 64) != 0) {
+                printf("Error: bit_width %zu is not multiple of 64\n", bit_width);
+            } else {
+                _width = bit_width/64;
+            }
+            _bit_width = bit_width;
+            _threshold = t_for_assign; 
+            if (_threshold == 0 ) {
+                printf("Threshold can't be 0, use default value 1.0\n");
+                _threshold = 1.0;
+            }
+            _centroids = (uint64_t *)malloc(K * _width * sizeof(uint64_t));
+            _newcentroids = (uint64_t *)malloc(K * _width * sizeof(uint64_t));
+            _counts = (int *)calloc(K, sizeof(int));
+            _bitsums =(int *)calloc(K*bit_width, sizeof(int));
+            if (_centroids == NULL || _newcentroids == NULL || _counts == NULL || _bitsums == NULL) {
+                printf("Allocate memory error\n");
+                exit(1);
+            }
+            assignFlag = false;
+            verbose = 0;
+        };
+
+        /* Train the data.
+            Returns:
+               0: normal
+              -1: exception 
+        */
+        int cluster();
+
+        /* compute WSSSE(Within Set Sum of Squared Errors) */
+        wssestruct computeCost();
+
+        /* Calculate the assignment for every point, a point may belong to multiple clusters */
+        std::vector< std::vector<unsigned int> > calc_assignment();
+
+        ~BKMeans();
+
+	    /* Returns a copy of the centroids */
+        std::vector<uint64_t *> get_centroids();
+
+        uint64_t *get_centroids_pointer();
+
+        /* find nearest center index */
+        int findNearestCenter(uint64_t *s);
+
+        /* find all nearest centers with same distance */
+        std::vector<unsigned int> findAllNearestCenters(uint64_t *s);
+
+        /* find nearest center first, then find all centers which distance difference 
+           is less than or equal to _threshold */
+        std::vector<unsigned int> findAllNearestCentersThreshold(uint64_t *s);
+
+        /* find nearest center first, then find all centers which distance difference 
+           is less than or equal to disthres */
+        std::vector<unsigned int> findAllNearestCentersDisthres(uint64_t *s, int disthres);
+
+        void setverbose(int level) { verbose = level;};
+
+        /* save trained model (centroids) to file */
+        int saveModel(const char *modelfile);
+        /* load trained model (centroids) from file */
+        int loadModel(const char *modelfile);
+
+        /* printf all distances */
+        void allDistances();
+        void allCentroidsDistances();
+        void mergeCentroids();
+
+        /* Computes Hamming distance between two samples */
+        unsigned int hamdist(uint64_t *x, uint64_t *y)
+        {
+		    unsigned int dist = 0;
+
+		    for (int i = 0; i < _width; i++)
+                dist += _mm_popcnt_u64(x[i] ^ y[i]);
+
+            return dist;
+	    }
+
+    private:
+        uint64_t *_centroids = NULL;
+        uint64_t *_newcentroids = NULL;
+        uint64_t *_samples = NULL;
+        int       _maxIters;
+        uint64_t  _costs; 
+        int       _k;
+        int       _n;
+        int       _width; /* how many uint64_t one binary data occupies */
+        int       _bit_width; /* how many bits one binary data occupies */
+        float     _threshold; /* in assignment, if second_best_centroid_distance -best_centroid_distance < _threshold,
+				 then the point belongs to 2 centroids */
+
+        int *_counts;   /* the number of samples belong to the cluster */
+        int *_bitsums;  /* bit counts for all samples belong to the cluster */
+
+        int verbose; /* verbose flag */
+        int epsilon; /* if the distance of new center and old center is less than epsilon,
+                            treat them as not changed and converged */
+        std::vector< std::vector<unsigned int> > _assignment; /* assignment for each point */
+        bool assignFlag; /* whether assignment already calculated */
+
+	    /* initialize random centroids */ 
+        int initRandCentroids();
+
+};
+
+
+namespace PlaceRecognizer {
+
+class BKMeans {
+
+public:
+BKMeans(uint K, uint iter_max);
+int cluster(cv::Mat &binary_data);
+/* compute WSSSE(Within Set Sum of Squared Errors) */
+wssestruct computeCost();
+
+/* Calculate the assignment for every point, a point may belong to multiple clusters */
+std::vector< std::vector<unsigned int> > calc_assignment();
+
+~BKMeans();
+
+/* Returns a copy of the centroids */
+cv::Mat get_centroids() const
+{ return _centroids.clone(); }
+
+/* find nearest center index */
+int findNearestCenter(uint sampleNo);
+
+/* find all nearest centers with same distance */
+std::vector<unsigned int> findAllNearestCenters(uint64_t *s);
+
+/* find nearest center first, then find all centers which distance difference
+   is less than or equal to _threshold */
+std::vector<unsigned int> findAllNearestCentersThreshold(uint64_t *s);
+
+/* find nearest center first, then find all centers which distance difference
+   is less than or equal to disthres */
+std::vector<unsigned int> findAllNearestCentersDisthres(uint64_t *s, int disthres);
+
+void setverbose(int level) { verbose = level;};
+
+/* save trained model (centroids) to file */
+int saveModel(const char *modelfile);
+/* load trained model (centroids) from file */
+int loadModel(const char *modelfile);
+
+/* printf all distances */
+void allDistances();
+void allCentroidsDistances();
+void mergeCentroids();
+
+/* Computes Hamming distance between two samples */
+unsigned int hamdist(uint64_t *x, uint64_t *y)
+{
+    unsigned int dist = 0;
+
+    for (int i = 0; i < _width; i++)
+        dist += _mm_popcnt_u64(x[i] ^ y[i]);
+
+    return dist;
+}
+
+uint hamdist(const cv::Mat &X, const cv::Mat &Y)
+{
+	uint d=0;
+	assert(X.cols==Y.cols and X.cols%8==0);
+	for (int i=0; i<X.cols/8; i++) {
+		uint64_t *x = (uint64_t*)(X.data + i*8),
+				 *y = (uint64_t*)(Y.data + i*8);
+		d += _mm_popcnt_u64(*x ^ *y);
+	}
+	return d;
+}
+
+/* --- */
+protected:
+cv::Mat
+	_centroids,
+	_newcentroids,
+	_samples;
+int       _maxIters;
+uint64_t  _costs;
+int       _k;
+int       _n;
+int       _width; /* how many uint64_t one binary data occupies */
+int       _bit_width; /* how many bits one binary data occupies */
+float     _threshold; /* in assignment, if second_best_centroid_distance -best_centroid_distance < _threshold,
+		 then the point belongs to 2 centroids */
+
+std::vector<uint> _counts;   /* the number of samples belong to the cluster */
+std::vector<uint> _bitsums;  /* bit counts for all samples belong to the cluster */
+
+int verbose = 0; /* verbose flag */
+int epsilon = 0; /* if the distance of new center and old center is less than epsilon,
+                    treat them as not changed and converged */
+std::vector< std::vector<unsigned int> > _assignment; /* assignment for each point */
+bool assignFlag = false; /* whether assignment already calculated */
+
+/* initialize random centroids */
+int initRandCentroids();
+
+};
+
+}	// namespace PlaceRecognizer
+
+
+#endif
---
+++ lib/BKMeans.cpp
@@ -0,0 +1,578 @@
+/*--------------------------------------
+ BKMeans for compact binary code
+
+    Compile option -I../include/libbkmeans -msse4.2 -fopenmp'
+    use environment variable OMP_NUM_THREADS to specify thread numbers
+-------------------------------------*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <errno.h>
+#include <cstdlib>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <ctime>
+#include <BKMeans.h>
+
+using namespace std;
+
+BKMeans::~BKMeans()
+{
+    free(_centroids);
+    free(_newcentroids);
+    free(_counts);
+    free(_bitsums);
+    _assignment.clear();
+}
+
+int BKMeans::findNearestCenter(uint64_t *s)
+{
+    int id = 0, mindist = 9999999, dist;
+    if (_centroids == NULL || _samples == NULL) {
+        cerr << "ERROR: centroids or samples are empty" << endl;
+        return -1; 
+    }
+
+    // Compute distance with all centroids
+    for (int i = 0; i < _k; i++) {
+        dist = hamdist(&_centroids[i*_width], s);
+        if (dist < mindist) {
+            mindist = dist;
+            id = i;
+        }
+    }
+
+    if (verbose > 2)
+        printf("BKMeans::findNearestCenter: %d %d\n", id, mindist);
+
+    return id;
+}
+
+std::vector<unsigned int> BKMeans::findAllNearestCenters(uint64_t *s)
+{
+    int id = 0, mindist = 9999999, dist;
+    std::vector<unsigned int> v;
+
+    // Compute distance with all centroids
+    for (int i = 0; i < _k; i++) {
+        dist = hamdist(&_centroids[i*_width], s);
+        if (dist < mindist) {
+            mindist = dist;
+            v.clear();
+            v.push_back(i);
+        } else if (dist == mindist) {
+            v.push_back(i);
+        }
+    }
+
+    return v;
+}
+
+/* centroid and distance */
+typedef struct centdist {
+    int centroid;
+    int dist;
+} centdist;    
+
+bool comparecd(centdist a, centdist b)
+{
+    return a.dist < b.dist;
+}
+
+std::vector<unsigned int> BKMeans::findAllNearestCentersThreshold(uint64_t *s)
+{
+    int mindist = 9999999, dist, id = 0;
+    int *distances = new int[_k];
+    centdist *cd = new centdist[_k];
+    std::vector<unsigned int> v;
+
+    // Compute distance with all centroids, find the minimum
+    for (int i = 0; i < _k; i++) {
+        dist = hamdist(&_centroids[i*_width], s);
+        if (verbose) 
+            printf(" %d", dist);
+        if (dist < mindist) {
+            mindist = dist;
+            id = i;
+        }
+        distances[i] = dist;
+    }
+    _counts[id]++;
+    if (verbose) 
+       printf("minid=%d mindist: %d\n", id, mindist);
+
+    // all the centroids which distance <= threshold
+    int num = 0;
+    for (int i = 0; i < _k; i++) {
+      if (distances[i] <= mindist/_threshold) {
+	    cd[num].dist = distances[i];
+	    cd[num].centroid = i;
+	    num++;
+	  }
+    }
+
+    std::sort(cd, cd+num, comparecd);
+    for (int i=0; i<num; i++) {
+        v.push_back(cd[i].centroid);
+    }
+
+    delete []distances;
+    delete []cd;
+    return v;
+}
+
+std::vector<unsigned int> BKMeans::findAllNearestCentersDisthres(uint64_t *s, int disthres)
+{
+    int mindist = 9999999, dist;
+    int *distances = new int[_k];
+    centdist *cd = new centdist[_k];
+    std::vector<unsigned int> v;
+
+    // Compute distance with all centroids, find the minimum
+    for (int i = 0; i < _k; i++) {
+        dist = hamdist(&_centroids[i*_width], s);
+        if (dist < mindist) {
+            mindist = dist;
+        }
+        distances[i] = dist;
+    }
+
+    // all the centroids which distance <= threshold
+    int num = 0;
+    for (int i = 0; i < _k; i++) {
+        if (distances[i] <= mindist+disthres) {
+	    cd[num].dist = distances[i];
+	    cd[num].centroid = i;
+	    num++;
+	}
+    }
+
+    std::sort(cd, cd+num, comparecd);
+    for (int i=0; i<num; i++) {
+        v.push_back(cd[i].centroid);
+    }
+
+    delete []distances;
+    delete []cd;
+    return v;
+}
+
+/* initialize the centroids randomly */
+int BKMeans::initRandCentroids()
+{
+    srand(time(NULL));
+
+    std::vector<uint32_t> tmp(_n);
+    for (int i = 0; i < _n; i++) {
+        tmp[i] = i;
+    }
+    // Shuffle   
+    std::random_shuffle (tmp.begin(), tmp.end());
+
+    if (verbose > 0)
+        printf("init centroids: ");
+    // Take K firsts
+    for (int i = 0; i < _k; i++) {
+        memcpy(_centroids+i*_width, _samples+tmp[i]*_width, _width*sizeof(uint64_t));
+        if (verbose > 0)
+            printf("%d ", tmp[i]);
+    }
+    if (verbose > 0)
+        printf("\n");
+    tmp.clear();
+
+    if (verbose > 0) {
+        for (int i = 0; i < _k; i++) {
+            printf("centroids[%d]: ", i);
+            for (int w=0; w<_width; w++) {
+                printf("%16lx ", _centroids[i*_width + w]);
+            }
+            printf("\n");
+        }
+    }
+
+    // OK
+    return 0;
+}
+
+/* trainning function */
+int BKMeans::cluster()
+{
+    bool stop;
+    int iter = 0, retcode = 0;
+    int bestcenter;
+
+    int i,j,w;
+
+    time_t begintime, endtime;
+    time(&begintime);
+    // Init: choose initial centroids
+    int iret = initRandCentroids();
+    if (iret != 0) {
+        cerr << "ERROR: cannot init centroids" << endl;
+        return -1;
+    }
+
+#if 0
+    for (i=0; i<_k*_width*2; i++) {
+        *(int *)((char *)_centroids+ i*4) = rand();
+    }
+#endif
+
+    stop = false;
+    iter = 0;
+    while (!stop) {
+        memset(_counts, 0, _k*sizeof(int));
+        memset(_bitsums, 0, _k*_bit_width*sizeof(int));
+        // Assign samples to centroids and compute bitsums
+        
+        #pragma omp parallel for private(bestcenter, w, j, i) 
+        for (i = 0; i < _n; i++) {
+            bestcenter = findNearestCenter(_samples + i*_width);
+            #pragma omp atomic
+            _counts[bestcenter]++;
+            
+            if (verbose > 2) {
+                printf("sample %d bestcenter=%d counts:%d\n", i+1, bestcenter, _counts[bestcenter]);
+            }
+
+            for (w=0; w<_width; w++) {
+                for (j=0; j<64; j++) {
+                    if (_samples[i*_width+w] & (1L<<j)) {
+                        #pragma omp atomic
+                        _bitsums[bestcenter*_bit_width + w*64 +j]++;
+                    }
+                }
+            }
+        }
+
+        if (verbose > 1) {
+            for (i=0; i<_k; i++) {
+                printf("bitsum[%d]: ", i);
+                for (j=0; j<_bit_width; j++) {
+                    printf("%d ", _bitsums[i*_bit_width+j]);
+                }
+                printf("\n");
+            }
+        }
+
+        // Find new centroids
+        int updates = 0;
+        memset(_newcentroids, 0, _k*_width*sizeof(uint64_t));
+
+        int bitsum2;
+        #pragma omp parallel for private(bitsum2, i, w, j) 
+        for (int i = 0; i < _k; i++) {
+            if (verbose > 0) 
+                printf("centers[%d] counts:%d\n", i, _counts[i]);
+            if (_counts[i] == 0) {
+                memset(&_centroids[i*_width], 0, _width*sizeof(uint64_t));
+                continue;
+            }
+            for (int w=0; w<_width; w++) {
+                for (int j=0; j<64; j++) {
+                    bitsum2 = _bitsums[i*_bit_width + w*64 +j] * 2;
+                    if (bitsum2 >= _counts[i]) {
+                        #pragma omp atomic
+                        _newcentroids[i*_width +w] |= 1L<<j;
+                    }
+                }
+            }
+            if (hamdist(&_newcentroids[i*_width], &_centroids[i*_width]) > epsilon) {
+                if (verbose)
+                    printf("--- centroid %d changed ---\n", i);
+                updates++;
+            }
+            memcpy(&_centroids[i*_width], &_newcentroids[i*_width], _width*sizeof(uint64_t));
+        }
+
+        if (verbose > 0) {
+            for (int i = 0; i < _k; i++) {
+                printf("new centroids[%d]: ", i);
+                for (int w=0; w<_width; w++) {
+                    printf("%16lx ", _newcentroids[i*_width + w]);
+                }
+                printf("\n");
+            }
+        }
+
+        if (verbose)
+            allCentroidsDistances();
+
+        //mergeCentroids();
+
+        wssestruct w = computeCost();
+        printf("----------- Iteration: %d, WSSSE: %f %f ----------\n", iter+1, w.cost1, w.cost2);
+        // Evaluate new cost and check stop condition
+        if (updates == 0) {
+            stop = true;
+            printf("\nINFO: algorithm fully converged in %d iterations\n", iter+1);
+            retcode = BKMEANS_RET_CONV;
+        }
+
+        // New iteration finished
+        iter++;
+        if (iter >= _maxIters) {
+            stop = true;
+            cout << endl << "INFO: max iterations reached" << endl;
+            retcode = BKMEANS_RET_MAXIT;
+        }
+    }
+    time(&endtime);
+    printf("--------- Train finished in %ld seconds -----------\n", endtime+1-begintime);
+
+    return retcode;
+}
+
+std::vector<uint64_t *> BKMeans::get_centroids()
+{
+        std::vector<uint64_t *> centroids;
+
+        for (int i = 0; i < _k; i++) {
+            centroids.push_back(_centroids+i*_width);
+        }
+
+        return centroids;
+}
+
+uint64_t  *BKMeans::get_centroids_pointer()
+{
+    return _centroids;
+}
+
+std::vector< std::vector<unsigned int> > BKMeans::calc_assignment()
+{
+    if (assignFlag)
+        return _assignment;
+
+    memset(_counts, 0, _k*sizeof(int));
+    std::vector<unsigned int> v;
+    for (int i = 0; i < _n; i++) {
+        if (verbose)
+            printf("i=%d ", i+1);
+        _assignment.push_back(findAllNearestCentersThreshold(_samples + i*_width));
+    }
+    
+    if (verbose) {
+        for (int i=0; i<_k; i++) {
+            printf("i=%d, count=%d\n", i, _counts[i]);
+        }
+    }
+
+    assignFlag = true;
+    return _assignment;
+}
+
+void BKMeans::allDistances()
+{
+    int i, j;
+
+    for (i=0; i<_n; i++) {
+        for (j=0; j<_n; j++) {
+            printf("%d ", hamdist(_samples+i*_width, _samples+j*_width));
+        }
+        printf("\n");
+    }
+}
+
+void BKMeans::allCentroidsDistances()
+{
+    int i, j, dist;
+
+    for (i=0; i<_k; i++) {
+        for (j=0; j<_k; j++) {
+            dist = hamdist(_centroids+i*_width, _centroids+j*_width);
+            printf("%4d ", dist);
+        }
+        printf("\n");
+    }
+}
+
+
+void BKMeans::mergeCentroids()
+{
+    int i, j, dist;
+
+    for (i=0; i<_k; i++) {
+        for (j=0; j<_k; j++) {
+            dist = hamdist(_centroids+i*_width, _centroids+j*_width);
+            if (j != i && dist < 10) {
+                printf("--- two centroid %d %d dist(%d) too close, converge to one centroid\n", j ,i, dist);
+                memset(_centroids+j*_width, 0, _width*8);
+            }
+        }
+    }
+}
+
+/* compute WSSSE(Within Set Sum of Squared Errors) */
+wssestruct BKMeans::computeCost()
+{
+    double cost;
+    wssestruct wsse;
+    wsse.cost1 = 0;
+    wsse.cost2 = 0;
+
+    int bestcenter, i;
+    #pragma omp parallel for private(bestcenter, cost) 
+    for (int i=0; i<_n; i++) {
+        bestcenter = findNearestCenter(_samples + i*_width);
+        cost = hamdist(&_samples[i*_width], &_centroids[bestcenter*_width]);
+        #pragma omp atomic
+        wsse.cost1 += cost;
+        #pragma omp atomic
+        wsse.cost2 += cost*cost;
+    }
+
+    wsse.cost2 = sqrt(wsse.cost2);
+    return wsse;
+}
+
+
+/* save trained model (centroids) to file */
+int BKMeans::saveModel(const char *modelfile)
+{
+    FILE *f = fopen(modelfile, "w");
+    if (f == NULL) {
+        printf("Model file %s can't be opened for write: %s\n", modelfile, strerror(errno));
+        return -1;
+    }
+
+    for (int i=0; i<_k; i++) {
+        for (int w=0; w<_width; w++) {
+            fprintf(f, "%16lx ", _centroids[i*_width + w]);
+        }
+        fprintf(f, "\n");
+    } 
+
+    fclose(f);
+    return 0;
+}
+
+/* load trained model (centroids) from file */
+int BKMeans::loadModel(const char *modelfile)
+{
+    ifstream fin(modelfile);
+    if (!fin) { std::cerr << "Error opening model file!\n"; return -1; }
+
+    string s;
+    int n=0;
+    while (fin >> s) {
+        _centroids[n++] = strtoull(s.c_str(), NULL, 16);
+        if (n > _k * _width) {
+            _k *= 2;
+            _centroids = (uint64_t *)realloc(_centroids, _k * _width * sizeof(uint64_t));
+        }
+    }
+    if (_k > n/_width)
+        _k = n/_width;
+
+    printf("k=%d \n", _k);
+        
+    return 0;
+}
+
+
+
+
+namespace PlaceRecognizer {
+
+BKMeans::BKMeans(uint K, uint iter_max) :
+	_maxIters(iter_max),
+	_k(K),
+	_threshold(1.0),
+	_counts(K, 0)
+{}
+
+
+int
+BKMeans::cluster(cv::Mat &binary_data)
+{
+	// Check & preparation
+	// Make sure data type is unsigned 8-bit integer
+	// with bit-width as multiples of 64
+	int m_type = binary_data.type() & CV_MAT_DEPTH_MASK;
+	assert(m_type==cv::DataType<uint8_t>::type);
+	assert(binary_data.channels()==1);
+
+	_n = binary_data.rows;
+	_samples = binary_data;
+	_bit_width = _samples.cols * 8;
+	if (_bit_width%64 != 0)
+		throw runtime_error("Matrix column is not multiples of 64-bit");
+	_width = _samples.cols;
+
+	_centroids = cv::Mat::zeros(_k, _width, CV_8U);
+	_newcentroids = cv::Mat::zeros(_k, _width, CV_8U);
+	_counts = std::vector<uint>(_k, 0);
+	_bitsums = std::vector<uint>(_k * _bit_width, 0);
+
+    // Initialize centroids
+    initRandCentroids();
+
+    bool stop;
+    int iter = 0, retcode = 0;
+    int bestcenter;
+
+    int i,j,w;
+    while (!stop) {
+
+    	std::fill(_counts.begin(), _counts.end(), 0);
+    	std::fill(_bitsums.begin(), _bitsums.end(), 0);
+
+        // Assign samples to centroids and compute bitsums
+        #pragma omp parallel for private(bestcenter, w, j, i)
+    	for (i=0; i<_n; i++) {
+    		bestcenter = findNearestCenter(i);
+
+    		#pragma omp atomic
+    		_counts[bestcenter]++;
+    	}
+    }
+}
+
+int
+BKMeans::initRandCentroids()
+{
+	srand(time(NULL));
+
+	std::vector<uint32_t> tmp(_n);
+	for (int i = 0; i < _n; i++) {
+		tmp[i] = i;
+	}
+	// Shuffle
+	std::random_shuffle (tmp.begin(), tmp.end());
+
+	if (verbose > 0)
+		printf("init centroids: ");
+	// Take K firsts
+	for (int i = 0; i < _k; i++) {
+		_centroids.row(i) = _samples.row(i);
+	}
+
+	// OK
+	return 0;
+
+}
+
+
+int BKMeans::findNearestCenter(uint sampleNo)
+{
+	int id=0, mindist = std::numeric_limits<int>::max(), dist;
+	auto curSample = _samples.row(sampleNo);
+
+	for (int i=0; i<_k; i++) {
+		dist = hamdist(_centroids.row(i), curSample);
+		if (dist < mindist) {
+			mindist = dist;
+			id = i;
+		}
+	}
+
+	return id;
+}
+
+}	// namespace PlaceRecognizer
+
+
