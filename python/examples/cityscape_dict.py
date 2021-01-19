261c29412650cc2f75d67a922840d920fc86fb6c
Adi Sujiwo <sujiwo@gmail.com>
2021-01-19 14:53:04 +0900

    Randomized samples for Cityscape

--- python/examples/cityscape_dict.py
+++ python/examples/cityscape_dict.py
@@ -8,6 +8,21 @@ from argparse import ArgumentParser
 from glob import glob
 from place_recognizer import VisualDictionary
 from tqdm import tqdm
+import random
+
+
+class CityscapeDataset:
+    def __init__(self, path_to_cityscape):
+        self.path = path_to_cityscape
+        self.allImageFiles = glob(path_to_cityscape + "/leftImg8bit/*/*/*png")
+        self.resize = 1.0
+        
+    def __len__(self):
+        return len(self.allImageFiles)
+    
+    def __getitem__(self, i):
+        img = cv2.imread(self.allImageFiles[i])
+        return cv2.resize(img, (0,0), None, fx=self.resize, fy=self.resize)
 
 
 if __name__=="__main__":
@@ -16,15 +31,19 @@ if __name__=="__main__":
     parser.add_argument("source_dataset", type=str, metavar="path_to_cityscape_dataset")
     parser.add_argument("output", type=str)
     parser.add_argument("--resize", type=float, default=1.0, metavar="factor", help="Resize images with this factor")
+    parser.add_argument("--random", type=int, default=-1, metavar="R", help="Only take R random samples from dataset")
     cmdArgs = parser.parse_args()
 
-    allImageFiles = glob(cmdArgs.source_dataset + "/leftImg8bit/*/*/*png")
+    dataset = CityscapeDataset(cmdArgs.source_dataset)
     visdict = VisualDictionary(numWords=256, numFeaturesOnImage=6000)
     
     print("Reading all images...")
-    for f in tqdm(allImageFiles):
-        img = cv2.imread(f)
-        img = cv2.resize(img, (0,0), None, fx=cmdArgs.resize, fy=cmdArgs.resize)
+    if cmdArgs.random!=-1:
+        samples = random.sample(dataset, cmdArgs.random)
+    else:
+        samples = dataset
+        
+    for img in tqdm(samples):
         visdict.train(img)
         cv2.imshow('image', img)
         cv2.waitKey(1)
