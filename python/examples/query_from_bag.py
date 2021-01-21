cd37503d2d1abab45e77e3329229c40c29683ac1
Adi Sujiwo <sujiwo@gmail.com>
2021-01-21 20:00:49 +0900

    Limit query

--- python/examples/query_from_bag.py
+++ python/examples/query_from_bag.py
@@ -14,7 +14,8 @@ from geodesy import utm
 import cv2
 import sys
 from argparse import ArgumentParser
-from place_recognizer import IncrementalBoW, VisualDictionary, VLAD2, VLADLoadError
+from place_recognizer import GenericImageDatabase
+from place_recognizer.GenericImageMap import getEnhancementMethods
 
 
 resizeFactor = 0.533333333
@@ -26,8 +27,6 @@ def processQuery(i):
     img=queryBag[i]
     bagLock.release()
     
-    img = cv2.resize(img, (0,0), None, fx=resizeFactor, fy=resizeFactor)
-    k,d = orb.detectAndCompute(img, None)
     return mapsource.query(d, numOfImages=50)
 
 if __name__=="__main__":
@@ -37,27 +36,35 @@ if __name__=="__main__":
     parser.add_argument("topic", type=str)
     parser.add_argument("mapfile", type=str)
     parser.add_argument("output", type=str)
+    parser.add_argument("--start", type=float, metavar="second", default=0, help="Start query from offset")
+    parser.add_argument("--stop", type=float, metavar="second", default=-1, help="Stop query at offset from 0")
+    
+    enhanceMethods = getEnhancementMethods()
+    if len(enhanceMethods)!=0:
+        parser.add_argument("--ime", type=str, choices=enhanceMethods, help='Preprocess image with this method')
+    else:
+        print("Enhancement not available; install im_enhance if you want")
+
     cmdArgs = parser.parse_args()
 
     queryBag = ImageBag(cmdArgs.bagfile, cmdArgs.topic)
     mapsource = None
     
-    try:
-        mapsource = VLAD2.load(cmdArgs.mapfile)
-        print("Map is of VLAD type")
-    except VLADLoadError:
-        mapsource = IncrementalBoW()
-        mapsource.load(cmdArgs.mapfile)
-        print("Map is of IBoW type")
-    except Exception:
-        print("Unable to determine map type")
-        exit(-1)
+    mapsource = GenericImageDatabase(cmdArgs.mapfile)
+    
+    if hasattr(cmdArgs, 'ime') and (cmdArgs.ime is not None):
+        from place_recognizer.GenericImageMap import ime
+        mapsource.useEnhancement = True
+        mapsource.enhanceMethod = eval('ime.' + prog_arguments.ime) 
+    else:
+        imeMethod = False
     
     bagLock = Lock()
     orb = cv2.ORB_create(6000)
     
     print("Ready")
-    pool4 = mlc.SuperPool(n_cpu=4)
-    positions = pool4.map(processQuery, range(len(queryBag)))
+    pool4 = mlc.SuperPool(n_cpu=1)
+    samples = queryBag.desample(hz=-1, True, cmdArgs.start, cmdArgs.stop)
+    positions = pool4.map(processQuery, samples)
     mlc.save(positions, cmdArgs.output)
     print("Done")
