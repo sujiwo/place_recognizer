Correct quaternion

python/GeographicCoordinate.py
@@ -145,12 +145,12 @@ class GeographicTrajectory:
                 m = pynmea2.parse(rawmsg.sentence)
                 coord = utm.fromLatLong(float(m.latitude), float(m.longitude), float(m.altitude))
                 gcoord = [coord.easting, coord.northing, coord.altitude, 0, 0, 0, 0]
-                if i>1:
+                if len(parsedCoordinates) > 1:
                     quat = GeographicTrajectory.orientationFromPositionOnlyYaw(parsedCoordinates[-1], parsedCoordinates[-2])
                     gcoord[3:7] = quat
                 parsedCoordinates.append(gcoord)
                 timestamps.append(rawmsg.header.stamp)
-            except:
+            except (AttributeError, TypeError, pynmea2.SentenceTypeError):
                 continue
         parsedCoordinates = np.array(parsedCoordinates)
         return timestamps, parsedCoordinates
@@ -202,5 +202,4 @@ if __name__=='__main__' :
     ps = GeographicTrajectory._parseFromNmea(tgbag)
     pass
     
-    
-    
+